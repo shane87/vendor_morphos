55717bad9787403d608eeb599b8a3837f95a54a1
Eric Clothier <eric.clothier@gmail.com>
2019-11-04 15:18:40 -0500

    missed some lineage stuff while Morph-izing

--- build/soong/generator/variables.go
+++ build/soong/generator/variables.go
@@ -6,12 +6,12 @@ import (
 	"android/soong/android"
 )
 
-func lineageExpandVariables(ctx android.ModuleContext, in string) string {
-	lineageVars := ctx.Config().VendorConfig("lineageVarsPlugin")
+func morphosExpandVariables(ctx android.ModuleContext, in string) string {
+	morphosVars := ctx.Config().VendorConfig("morphosVarsPlugin")
 
 	out, err := android.Expand(in, func(name string) (string, error) {
-		if lineageVars.IsSet(name) {
-			return lineageVars.String(name), nil
+		if morphosVars.IsSet(name) {
+			return morphosVars.String(name), nil
 		}
 		// This variable is not for us, restore what the original
 		// variable string will have looked like for an Expand
